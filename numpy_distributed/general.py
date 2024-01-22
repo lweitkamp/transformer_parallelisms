0237fb4b2dc57c4c3c723506205500278fb8490f
laurensweitkamp <laurensweitkamp@thirona.eu>
2024-01-22 20:35:45 +0100

    column/row linear working

--- numpy_distributed/general.py
+++ numpy_distributed/general.py
@@ -10,3 +10,10 @@ def world_size() -> int:
     """Return the world size, i.e. the number of parallel programs."""
     comm = MPI.COMM_WORLD
     return comm.Get_size()
+
+
+def assert_divisible(dim: int) -> None:
+    """A simple assert to check that whatever the size of the
+    dimension is, it can be equally divided among devices."""
+    assert dim % world_size() == 0, \
+        f"Cannot divide the dimension {dim} amongst {world_size()} devices."
--- tensor_parallel/linear.py
+++ tensor_parallel/linear.py
@@ -18,27 +18,34 @@ class Linear:
 
 
 class ColumnParallelLinear:
-    """A linear layer scattered along the columns."""
+    """A linear layer scattered along the column dimension."""
 
     def __init__(self, d_model: int, d_hidden: int, rng):
+        ndist.assert_divisible(d_hidden)
         self.weight = rng.random((d_model, d_hidden // ndist.world_size()))
         self.bias = rng.random((d_hidden // ndist.world_size(), ))
 
     def forward(self, x: np.ndarray) -> np.ndarray:
-        return self.weight @ x + self.bias
+        """Compute the matrix product x @ W + b. The bias is also scattered
+        and hence we have to add it for every device."""
+        return x @ self.weight + self.bias
 
     def backward(self):
         raise NotImplementedError
 
 
 class RowParallelLinear:
-    """A linear layer scattered along the rows."""
+    """A linear layer scattered along the row dimension."""
 
     def __init__(self, d_model: int, d_hidden: int, rng):
+        ndist.assert_divisible(d_model)
         self.weight = rng.random((d_model // ndist.world_size(), d_hidden))
         self.bias = rng.random((d_hidden, ))
 
     def forward(self, x: np.ndarray) -> np.ndarray:
+        """Compute the matrix product x @ W + b. Since the weights are
+        scatterd along the row dimension the bias is identical for each
+        device. Hence, we only add the bias for a single device."""
         out = x @ self.weight
 
         if ndist.rank() == 0:
