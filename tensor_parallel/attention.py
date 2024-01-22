bbc75948bfbfb589f7c8b7163f1e340788426b3d
laurensweitkamp <laurensweitkamp@thirona.eu>
2024-01-22 21:22:04 +0100

    refactor attention to include non-parallel version

--- tensor_parallel/attention.py
+++ tensor_parallel/attention.py
@@ -12,22 +12,12 @@ def softmax(x: np.ndarray, axis: int = -1) -> np.ndarray:
 class Attention:
     """A Multi-headed self-Attention (decoder-only) layer."""
 
-    def __init__(self, d_model: int, n_heads_chunk: int, d_hidden: int, rng):
-        """Initiate weights for the Attention layer. We split across
-        /heads/ to leave the attention values valid."""
-        self.q, self.k, self.v = rng.random(
-            (3, d_model, n_heads_chunk, d_hidden)
-        )
-        self.b = rng.random(
-            (n_heads_chunk, d_hidden, d_model)
-        )
+    def __init__(self, d_model: int, n_heads: int, d_hidden: int, rng):
+        self.q, self.k, self.v = rng.random((3, d_model, n_heads, d_hidden))
+        self.b = rng.random((n_heads, d_hidden, d_model))
 
     def forward(self, x: np.ndarray) -> np.ndarray:
-        """Forward pass through the self-attention layer.
-
-        The operations here are scattered on the head dimension, so an
-        all-reduce is required at the end.
-        """
+        """Forward pass through the self-attention layer."""
         # b: batch, s: seq len, d: d_model, h: num heads, m: d_hidden
         q = np.einsum("bsd, dhm -> bshm", x, self.q)
         k = np.einsum("bsd, dhm -> bshm", x, self.k)
@@ -36,9 +26,25 @@ class Attention:
         attention = softmax(np.einsum("bshm, bzhm -> bhsz", q, k), axis=-1)
         y = np.einsum("bhss, bshm -> bshm", attention, v)
         z = np.einsum("bshm, hmd -> bsd", y, self.b)
-        ndist.all_reduce(z)
-
         return z
+
+    def backward(self):
+        """Backward pass through the Attention layer."""
+        raise NotImplementedError
+
+
+class HeadParallelAttention(Attention):
+    """A Multi-headed self-Attention (decoder-only) layer. We split the
+    weights over multiple devices along the head dimension."""
+
+    def __init__(self, d_model: int, n_heads: int, d_hidden: int, rng):
+        ndist.assert_divisible(n_heads)
+        super().__init__(
+            d_model=d_model,
+            n_heads=n_heads // ndist.world_size(),
+            d_hidden=d_hidden,
+            rng=rng,
+        )
 
     def backward(self):
         """Backward pass through the Attention layer."""
