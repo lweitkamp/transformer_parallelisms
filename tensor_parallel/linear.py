478ce8028f965278b9dbaa776bdbf76cda1c4cb4
laurensweitkamp <laurensweitkamp@thirona.eu>
2024-01-22 21:47:07 +0100

    Simplify parallel linear layers

--- tensor_parallel/linear.py
+++ tensor_parallel/linear.py
@@ -17,13 +17,16 @@ class Linear:
         raise NotImplementedError
 
 
-class ColumnParallelLinear:
+class ColumnParallelLinear(Linear):
     """A linear layer scattered along the column dimension."""
 
     def __init__(self, d_model: int, d_hidden: int, rng):
         ndist.assert_divisible(d_hidden)
-        self.weight = rng.random((d_model, d_hidden // ndist.world_size()))
-        self.bias = rng.random((d_hidden // ndist.world_size(), ))
+        super().__init__(
+            d_model=d_model,
+            d_hidden=d_hidden // ndist.world_size(),
+            rng=rng,
+        )
 
     def forward(self, x: np.ndarray) -> np.ndarray:
         """Compute the matrix product x @ W + b. The bias is also scattered
@@ -34,13 +37,16 @@ class ColumnParallelLinear:
         raise NotImplementedError
 
 
-class RowParallelLinear:
+class RowParallelLinear(Linear):
     """A linear layer scattered along the row dimension."""
 
     def __init__(self, d_model: int, d_hidden: int, rng):
         ndist.assert_divisible(d_model)
-        self.weight = rng.random((d_model // ndist.world_size(), d_hidden))
-        self.bias = rng.random((d_hidden, ))
+        super().__init__(
+            d_model=d_model // ndist.world_size(),
+            d_hidden=d_hidden,
+            rng=rng,
+        )
 
     def forward(self, x: np.ndarray) -> np.ndarray:
         """Compute the matrix product x @ W + b. Since the weights are
