9b1e9c0e31e36e7eba336c284621f7580bda94b6
laurensweitkamp <laurensweitkamp@thirona.eu>
2024-01-29 19:03:05 +0100

    refactor relu and add backward pass

--- numpy_sequential/mlp.py
+++ numpy_sequential/mlp.py
@@ -7,14 +7,18 @@ class MLP:
     """Simple Multi-Layered Perceptron with two layers."""
 
     def __init__(self, d_model: int, d_hidden: int, rng):
-        self.w1 = nseq.Linear(d_model=d_model, d_hidden=d_hidden, rng=rng)
-        self.w2 = nseq.Linear(d_model=d_hidden, d_hidden=d_model, rng=rng)
+        self.layers = [
+            nseq.Linear(d_model=d_model, d_hidden=d_hidden, rng=rng),
+            nseq.ReLU(),
+            nseq.Linear(d_model=d_hidden, d_hidden=d_model, rng=rng),
+        ]
 
     def forward(self, inputs: np.ndarray) -> np.ndarray:
-        x = self.w1.forward(inputs)
-        x = np.maximum(0, x)
-        x = self.w2.forward(x)
-        return x
+        for layer in self.layers:
+            inputs = layer.forward(inputs)
+        return inputs
 
-    def backward(self):
-        raise NotImplementedError
+    def backward(self, grads: np.ndarray) -> np.ndarray:
+        for layer in self.layers[::-1]:
+            grads = layer.backward(grads)
+        return grads
