0bdb8f1273705657af7c74880ed57027e2c45335
laurens <laurensweitkamp@thirona.eu>
2024-02-11 13:46:23 +0100

    add causal mask and Keep It Simple

--- numpy_sequential/attention.py
+++ numpy_sequential/attention.py
@@ -9,11 +9,11 @@ class Attention:
     def __init__(
         self, d_model: int, n_heads: int, d_hidden: int, rng, dtype=np.float32
     ):
-        self.q = npseq.Linear(d_model, (n_heads, d_hidden), rng, dtype)
-        self.k = npseq.Linear(d_model, (n_heads, d_hidden), rng, dtype)
-        self.v = npseq.Linear(d_model, (n_heads, d_hidden), rng, dtype)
-        self.b = npseq.Linear((n_heads, d_hidden), d_model, rng, dtype)
+        self.in_proj = npseq.Linear(d_model, (3 * n_heads, d_hidden), rng, dtype)
+        self.out_proj = npseq.Linear((n_heads, d_hidden), d_model, rng, dtype)
         self.softmax = npseq.Softmax(axis=-1)
+
+        self.scale = np.sqrt(d_hidden)
 
         self.ctx: dict = {"attention": None, "q": None, "k": None, "v": None}
 
@@ -21,41 +21,45 @@ class Attention:
         """Forward pass through the self-attention layer."""
         self.ctx["inputs"] = inputs
 
-        q, k, v = self.q.forward(inputs), self.k.forward(inputs), self.v.forward(inputs)
+        q, k, v = np.split(self.in_proj.forward(inputs), 3, axis=2)
 
-        attention_weights = np.einsum("bshm, bzhm -> bhsz", q, k)
+        attention_weights = np.einsum("bshm, bzhm -> bhsz", q, k) / self.scale
+        _, _, s1, s2 = attention_weights.shape
+        mask = np.expand_dims(np.tri(s1, s2, dtype=bool), (0, 1))
+
+        attention_weights = np.where(mask, attention_weights, float("-inf"))
         attention = self.softmax.forward(attention_weights)
 
         y = np.einsum("bhss, bshm -> bshm", attention, v)
-        z = self.b.forward(y)
+        out = self.out_proj.forward(y)
 
         self.ctx["attention"] = np.copy(attention)
+        self.ctx["mask"] = np.copy(mask)
         self.ctx["q"] = np.copy(q)
         self.ctx["k"] = np.copy(k)
         self.ctx["v"] = np.copy(v)
 
-        return z
+        return out
 
     def backward(self, grads: np.ndarray) -> np.ndarray:
         """Backward pass through the Attention layer."""
+        grads = self.out_proj.backward(grads)
 
-        grads = self.b.backward(grads)
-
-        grads_v = np.einsum("bsmh, bhsz -> bshm", grads, self.ctx["attention"])
-        grads = np.einsum("bsmh, bzhm -> bhsz", grads, self.ctx["v"])
+        grads_v = np.einsum("bshm, bhsz -> bshm", grads, self.ctx["attention"])
+        grads = np.einsum("bshm, bzhm -> bhsz", grads, self.ctx["v"])
 
         grads = self.softmax.backward(grads)
+        grads = np.where(self.ctx["mask"], grads, 0) / self.scale
 
         grads_q = np.einsum("bhsz, bshm -> bshm", grads, self.ctx["k"])
         grads_k = np.einsum("bhsz, bshm -> bzhm", grads, self.ctx["q"])
 
-        grads = (
-            self.q.backward(grads_q)
-            + self.k.backward(grads_k)
-            + self.v.backward(grads_v)
+        grads = self.in_proj.backward(
+            np.concatenate([grads_q, grads_k, grads_v], axis=2)
         )
 
         self.ctx["attention"] = None
+        self.ctx["mask"] = None
         self.ctx["q"] = None
         self.ctx["k"] = None
         self.ctx["v"] = None
