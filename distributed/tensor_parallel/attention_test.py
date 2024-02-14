e9eb0dbaacbb20a5004cd19fd230111676101081
laurens <laurensweitkamp@thirona.eu>
2024-02-14 20:46:52 +0100

    most shapes pass

--- distributed/tensor_parallel/attention_test.py
+++ distributed/tensor_parallel/attention_test.py
@@ -7,7 +7,12 @@ from layers import Attention
 
 @pytest.mark.parametrize(
     "batch_size,seq_len,d_model,n_heads,seed",
-    [(1, 2, 4, 2, 42), (2, 4, 8, 4, 42)],
+    [
+        (1, 1, 8, 2, 42),
+        (3, 1, 8, 2, 42),
+        (1, 3, 8, 2, 42),
+        (3, 3, 8, 2, 42),
+    ],
 )
 def test_attention(
     batch_size: int,
@@ -19,26 +24,30 @@ def test_attention(
     """Create a sequential attention layer and scatter the values to a
     head-parallel version. Compare outputs of a generated input,
     the outputs should match."""
-    global_rng = np.random.default_rng(seed)
-    local_rng = np.random.default_rng(seed + npdist.rank() + 1)
+    rng = np.random.default_rng(seed)
 
     # Create a normal- and a head parallel attention-layer.
-    attention = Attention(d_model, n_heads, d_model, global_rng)
+    attention = Attention(d_model, n_heads, d_model, rng)
     head_attention = npdist.HeadParallelAttention(
         d_model,
         n_heads,
         d_model,
-        local_rng,
+        rng,
     )
 
-    # Scatter the attention layer's weights.
-    npdist.scatter(attention.in_proj, head_attention.in_proj, axis=1)
-    npdist.scatter(attention.q, head_attention.q, axis=1)
-    npdist.scatter(attention.k, head_attention.k, axis=1)
-    npdist.scatter(attention.v, head_attention.v, axis=1)
-    npdist.scatter(attention.b, head_attention.b, axis=0)
+    # (d_model, n_heads, d_hidden)
+    npdist.scatter(attention.q_proj.weight, head_attention.q_proj.weight, axis=1)
+    npdist.scatter(attention.k_proj.weight, head_attention.k_proj.weight, axis=1)
+    npdist.scatter(attention.v_proj.weight, head_attention.v_proj.weight, axis=1)
+    npdist.scatter(attention.q_proj.bias, head_attention.q_proj.bias, axis=0)
+    npdist.scatter(attention.k_proj.bias, head_attention.k_proj.bias, axis=0)
+    npdist.scatter(attention.v_proj.bias, head_attention.v_proj.bias, axis=0)
+    npdist.scatter(attention.out_proj.weight, head_attention.out_proj.weight, axis=0)
+    npdist.scatter(attention.out_proj.bias, head_attention.out_proj.bias, axis=0)
 
     # Init the input with the global seed.
-    x = global_rng.random((batch_size, seq_len, d_model))
+    x = rng.random((batch_size, seq_len, d_model))
+    y = np.ones_like(x)
 
     np.testing.assert_allclose(attention.forward(x), head_attention.forward(x))
+    np.testing.assert_allclose(attention.backward(y), head_attention.backward(y))
