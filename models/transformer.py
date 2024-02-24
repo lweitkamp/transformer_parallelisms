ce2546be88c8dcf4602b029ae4984ee61a95dc88
laurens <laurensweitkamp@thirona.eu>
2024-02-24 13:21:26 +0100

    class refactor

--- models/transformer.py
+++ models/transformer.py
@@ -2,8 +2,10 @@ import numpy as np
 
 import layers
 
+from layers.core import Block
 
-class Transformer:
+
+class Transformer(Block):
     def __init__(
         self,
         seq_len: int,
@@ -15,53 +17,24 @@ class Transformer:
         dtype=np.float32,
     ):
         """..."""
+        super().__init__()
 
-        # Input embeddings.
-        self.input_embedding = layers.InputEmbedding(d_model, vocab_size, rng)
-        self.pos_embed = layers.PositionalEmbedding(d_model, seq_len)
-
-        # Sequence of transformer blocks.
-        self.layers = [
-            layers.TransformerBlock(d_model, n_heads, rng, dtype) for _ in range(n_layers)
-        ]
-
-        # De-embedding
-        self.output_embedding = layers.OutputEmbedding(self.input_embedding.weights)
+        self.layers.extend([
+            layers.InputEmbedding(d_model, vocab_size, rng),
+            layers.PositionalEmbedding(d_model, seq_len),
+        ])
+        self.layers.extend([
+            layers.TransformerBlock(d_model, n_heads, rng, dtype)
+            for _ in range(n_layers)
+        ])
+        self.layers.append(layers.OutputEmbedding(self.layers[0].weight))
 
     def forward(self, inputs: np.ndarray) -> np.ndarray:
-        """..."""
-        inputs = self.input_embedding.forward(inputs)
-        inputs = self.pos_embed.forward(inputs)
         for layer in self.layers:
-            inputs = layer.forward(inputs)
-        inputs = self.output_embedding.forward(inputs)
+            inputs = layer(inputs)
         return inputs
 
     def backward(self, grads: np.ndarray) -> np.ndarray:
-        """..."""
-        grads = self.output_embedding.backward(grads)
         for layer in self.layers[::-1]:
             grads = layer.backward(grads)
-        grads = self.pos_embed.backward(grads)
-        grads = self.input_embedding.backward(grads)
         return grads
-
-
-if __name__ == "__main__":
-    rng = np.random.default_rng(42)
-
-    transformer = Transformer(
-        seq_len=128,
-        vocab_size=512,
-        n_layers=2,
-        d_model=256,
-        n_heads=8,
-        rng=rng,
-    )
-
-    out = transformer.forward(
-        rng.integers(0, 512, size=(1, 128))
-    )
-    transformer.backward(np.ones_like(out))
-
-    assert out.shape == (1, 128, 512)
