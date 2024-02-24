cb09f92bd8c0182b4aefc5d3ed21d2542bd9aa67
laurens <laurensweitkamp@thirona.eu>
2024-02-24 14:28:41 +0100

    work on test script

--- train.py
+++ train.py
@@ -1,6 +1,9 @@
 from pathlib import Path
+
 import models
+import optimizers
 import data
+import layers
 
 import numpy as np
 
@@ -16,18 +19,21 @@ def train(num_epoch: int = 100, seq_len: int = 32, batch_size: int = 16):
         batch_size=batch_size,
     )
 
-    # transformer = models.Transformer(
-    #     seq_len=128,
-    #     vocab_size=512,
-    #     n_layers=2,
-    #     d_model=256,
-    #     n_heads=8,
-    #     rng=rng,
-    # )
+    transformer = models.Transformer(
+        seq_len=32,
+        vocab_size=512,
+        n_layers=2,
+        d_model=64,
+        n_heads=8,
+        rng=rng,
+    )
+
+    optimizer = optimizers.Adam(transformer, layers.SoftmaxCrossEntropy(), 0.0001)
 
     for epoch in range(num_epoch):
         for input_data, labels in dataloader.iter_epoch():
-            ...
+            out = optimizer.step(input_data, labels)
+            print(epoch, out["loss"].sum())
 
 
 if __name__ == "__main__":
