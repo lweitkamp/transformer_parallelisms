3674bfa36b1f4aded00dd45df4bbef35310bbffe
laurens <laurensweitkamp@thirona.eu>
2024-02-25 14:29:09 +0100

    make adam not sgd

--- optimizers/adam.py
+++ optimizers/adam.py
@@ -3,19 +3,53 @@ import itertools
 
 
 class Adam:
-    def __init__(self, model, loss_fn: callable, learning_rate: float):
+    def __init__(
+        self,
+        model,
+        loss_fn: callable,
+        learning_rate: float,
+        betas=(0.9, 0.999),
+        eps=1e-08,
+    ):
         self.learning_rate = learning_rate
         self.loss_fn = loss_fn
         self.model = model
 
-        self.parameters = list(itertools.chain.from_iterable(model.expose()))
+        self.parameters = [
+            (
+                layer,
+                tensor_name,
+                np.zeros_like(getattr(layer, tensor_name)),  # velocity
+                np.zeros_like(getattr(layer, tensor_name)),  # momentum
+            )
+            for layer, tensor_name in list(
+                itertools.chain.from_iterable(model.expose())
+            )
+        ]
+        self.timestep = 0
+        self.betas = betas
+        self.eps = eps
 
     def update(self, norm: int):
-        for layer, tensor_name in self.parameters:
+        self.timestep += 1
+
+        b1, b2 = self.betas
+
+        for layer, tensor_name, velocity, momentum in self.parameters:
             grads = layer.grads[tensor_name]
             weight = getattr(layer, tensor_name)
-            update = weight + self.learning_rate * grads / norm
-            setattr(layer, tensor_name, update)
+
+            # in-place update of momentum and velocity
+            np.add(b1 * momentum, (1 - b1) * grads, out=momentum)
+            np.add(b2 * velocity, (1 - b2) * np.power(grads, 2), out=velocity)
+
+            m_hat = momentum / (1 - b1 ** self.timestep)
+            v_hat = velocity / (1 - b2 ** self.timestep)
+            update = self.learning_rate * m_hat / (np.sqrt(v_hat) + self.eps)
+
+            # TODO: we essentially sum up the grads for batch and seq dim, shouldn't
+            # we avg it at some point?
+            setattr(layer, tensor_name,  weight - update)
 
     def step(self, inputs: np.ndarray, labels: np.ndarray) -> dict:
         batch_size, seq_len, *_ = inputs.shape
